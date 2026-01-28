5edc4442c3782beaf20f15431ac40b65eab27321
alfirasafarini <safarinialfira321@gmail.com>
2026-01-28 08:48:42 +0700

    variable up

--- belajar.php/biodata.php
+++ belajar.php/biodata.php
@@ -1,4 +1,7 @@
 <?php
-    echo "Nama:Alfira <br>" ;
-    echo "Kelas:XI TJKT 2 di" ;
+    echo "Nama:Alfira Safarini<br>" ;
+    echo "TTL:31 Januari 2009 <br>" ;
+    echo "Alamat:Kp.Andir No 99 RT/RW 02/10 <br>" ;
+    echo "Instagram:@curlupbubles <br>" ;
+    echo "Alamat Email:safarinialfira321@gmail.com<br>" ;
 ?>
---
+++ belajar.php/variable.php
@@ -0,0 +1,25 @@
+<?php
+
+// tipe data char (karakter)
+$jenis_kelamin = 'P';
+
+// tipe data string (teks)
+$nama_lengkap = "Alfira Safarini";
+
+// tipe data integer
+$umur = 16;
+
+// tipe data float
+$berat = 44.3;
+
+// tipe data float
+$tinggi = 150.2;
+
+
+
+echo "Nama: $nama_lengkap<br>";
+echo "Jenis Kelamin: $jenis_kelamin<br>";
+echo "Umur: $umur tahun<br>";
+echo "berat badan: $berat kg<br>";
+echo "tinggi badan: $tinggi cm<br>";
+
