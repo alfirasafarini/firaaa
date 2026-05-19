c78d77fb26b795f8b162c984eb2c8155ef930b1a
alfirasafarini <safarinialfira321@gmail.com>
2026-05-19 21:12:41 +0700

    upd

--- osis.login/index.php
+++ osis.login/index.php
@@ -1,18 +1,9 @@
 <?php
-include 'koneksi.php';
 session_start();
 
-if (isset($_POST['login'])) {
-    $user = $_POST['username'];
-    $pass = $_POST['password'];
-
-    $query = mysqli_query($conn, "SELECT * FROM users WHERE username='$user' AND password='$pass'");
-    if (mysqli_num_rows($query) > 0) {
-        $_SESSION['user'] = $user;
-        header("Location: data_diri.php");
-    } else {
-        echo "<script>alert('Username atau Password Salah!');</script>";
-    }
+if(!isset($_SESSION['username'])){
+    header("Location: login.php");
+    exit;
 }
 ?>
 
